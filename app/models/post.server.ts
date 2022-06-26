87d69aceb95684db92b4f449a52eb86a4dad3530
projectormato <k.keita811.anuy@gmail.com>
2022-06-26 22:25:12 +0900

    feat: Post をAdminから新規作成まで実装

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,5 @@
+import type { Post } from "@prisma/client";
 import { prisma } from "~/db.server";
-
-export type { Post } from "@prisma/client";
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -8,4 +7,10 @@ export async function getPosts() {
 
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
 }
